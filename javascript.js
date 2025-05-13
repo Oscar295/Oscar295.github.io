dce758a3081d795a640a0925eecd6b60b762030e
Oscar295 <80195361+Oscar295@users.noreply.github.com>
2025-05-14 01:10:28 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+        x = document.getElementById("image");
+        console.log(previewPic.src);
+        console.log(x);
+        var altText = previewPic.alt;
+        x.style.backgroundImage = "url('" + previewPic.src + "')";
+        x.innerHTML = altText;
+       }
+   
+       function unDo(){
+           x = document.getElementById("image");
+           console.log(previewPic.src);
+           console.log(x);
+        x.style.backgroundImage = "url('')";
+        x.innerHTML = "Hover over an image below to display here.";
+       }
